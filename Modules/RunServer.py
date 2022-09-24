c058a7fbd388032c2f28ce6d809e9fb74ec45283
meaphamster <thedcat101@gmail.com>
2022-09-24 17:08:57 -0500

    Done.

--- Modules/RunServer.py
+++ Modules/RunServer.py
@@ -1,5 +1,6 @@
 import discord
 # from subprocess import Popen
+import time
 import os
 
 async def cmd_RunServer_Invoked(Member, interaction):
@@ -13,10 +14,37 @@ async def cmd_RunServer_Invoked(Member, interaction):
 
         # subprocess.call(["C:\\Users\\Fayas\\Desktop\\Minecraft_Server\\run.bat"])
 
+        embed_1 = discord.Embed(
+            title = 'Sending Request to Start...',
+            colour = discord.Colour.dark_grey()
+        )
+        embed_2 = discord.Embed(
+            title = 'Server Online',
+            colour = discord.Colour.dark_grey()
+        )
+
+        remainingTime = 60
+
+        # await interaction.response.send_message(content = 'Running server...')
+        await interaction.response.send_message(embed = embed_1)
+
         os.chdir(r"C:\Users\Fayas\Desktop\Minecraft_Server")
         os.startfile("run.bat")
 
-        await interaction.response.send_message(content = 'Running server...', ephemeral = False)
+        time.sleep(5)
+        
+        for i in range(remainingTime, 0, -10):
+            embed = discord.Embed(
+                title = 'Starting Server...',
+                description = str(i) + ' seconds remaining...',
+                colour = discord.Colour.dark_grey()
+            )
+            await interaction.edit_original_response(embed = embed)
+
+            time.sleep(10)
+        
+        await interaction.edit_original_response(embed = embed_2)
+
 
     else:
         
--- Modules/StopServer.py
+++ Modules/StopServer.py
@@ -6,7 +6,21 @@ async def cmd_StopServer_Invoked(Member, interaction):
 
     if Member.guild_permissions.administrator == True:
 
-        time.sleep(1)
+        embed_1 = discord.Embed(
+            title = 'Sending Request to Terminate...',
+            colour = discord.Colour.dark_grey()
+        )
+        embed_2 = discord.Embed(
+            title = 'Terminating Server...',
+            colour = discord.Colour.dark_grey()
+        )
+        embed_3 = discord.Embed(
+            title = 'Successfully Terminated Server',
+            colour = discord.Colour.dark_grey()
+        )
+
+        # await interaction.response.send_message(content = 'Sending Request to Terminate...')
+        await interaction.response.send_message(embed = embed_1)
 
         print("Stopping Server...")
 
@@ -15,9 +29,19 @@ async def cmd_StopServer_Invoked(Member, interaction):
         
         print("Server Terminated")
 
+        time.sleep(4)
+
+        # interactionResponse = await interaction.original_message()
+
+        # await interaction.edit_original_response(content = "Terminating Server...")
+        await interaction.edit_original_response(embed = embed_2)
+
+        time.sleep(4)
+
         pyautogui.write('a', interval = .1)
 
-        await interaction.response.send_message(content = 'Server stopped', ephemeral = False)
+        # await interaction.edit_original_response(content = "Successfully Terminated Server")
+        await interaction.edit_original_response(embed = embed_3)
 
     else:
         
