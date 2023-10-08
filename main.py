8abf8be86a189abdc1c8a51cfa20d16f2f3343f7
Meap <78829866+MeapHamster@users.noreply.github.com>
2023-10-08 10:39:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands, tasks
 import settings
 settings.init()
 
-token = 'MTAyMjY5ODQ5OTI5NDM2NzgxNQ.GLvmB7.LGuaNJVzCWSNyAEPr66Uw-gkwrYLjYOm_PDrDQ'
+token = ''
 
 class MyBot(commands.Bot):
     def __init__(self):
